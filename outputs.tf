22768fc6685253a2c0f159a8772cc7a939efd2ed
Dave Arnold <darnold@hashicorp.com>
2018-07-06 17:51:11 -0700

    adding outputs to link workspaces

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "fqdn" {
   value = "${azurerm_mysql_server.server.fqdn}"
 }
+
+output "server_name" {
+  value = "${var.server_name}"
+}
+
+output "db_name" {
+  value = "${var.db_name}"
+}
+
+output "vault_token" {
+  value = "${var.vault_token}"
+}
+
+output "vault_cluster" {
+  value = "${var.vault_cluster}"
+}
