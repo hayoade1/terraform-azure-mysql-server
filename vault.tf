a986183a4a67ca19c4cc5c934ef693ad94002c61
Dave Arnold <darnold@hashicorp.com>
2018-07-12 10:31:18 -0700

    adding flush privileges

--- vault.tf
+++ vault.tf
@@ -55,7 +55,7 @@ resource "vault_database_secret_backend_role" "mysql_crud" {
   backend             = "${vault_mount.db.path}"
   name                = "mysql_crud"
   db_name             = "${var.db_name}"
-  creation_statements = "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}'; grant CREATE,DROP,SELECT,INSERT,UPDATE,DELETE on ${var.db_name}.* to '{{name}}'@'%'"
+  creation_statements = "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}'; GRANT ALTER, CREATE ON ${var.db_name} to '{{name}}'@'%'; grant CREATE,DROP,SELECT,INSERT,UPDATE,DELETE on ${var.db_name}.* to '{{name}}'@'%'; flush privileges"
   default_ttl         = "${var.default_ttl}"
   max_ttl             = "${var.max_ttl}"
 }
@@ -64,7 +64,7 @@ resource "vault_database_secret_backend_role" "mysql_ro" {
   backend             = "${vault_mount.db.path}"
   name                = "mysql_ro"
   db_name             = "${var.db_name}"
-  creation_statements = "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}'; grant SELECT on ${var.db_name}.* to '{{name}}'@'%'"
+  creation_statements = "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}'; grant SELECT on ${var.db_name}.* to '{{name}}'@'%'; flush privileges"
   default_ttl         = "${var.default_ttl}"
   max_ttl             = "${var.max_ttl}"
 }
