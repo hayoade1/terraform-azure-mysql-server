ebbc8d5c9e8faf42d6ee2dfb78f0876f14c1a110
hayoade1 <44470559+hayoade1@users.noreply.github.com>
2020-02-15 15:37:34 -0500

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -56,7 +56,7 @@ locals {
       "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';",
       "GRANT ALTER, CREATE ON ${var.db_name} to '{{name}}'@'%';",
       "grant CREATE,DROP,SELECT,INSERT,UPDATE,DELETE on ${var.db_name}.* to '{{name}}'@'%';",
-      'flush privileges'"
+      "flush privileges"
   ]
 }
 
