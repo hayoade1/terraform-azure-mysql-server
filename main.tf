2b3f5cfe4c4f32fe69f49ee907da5c407f7e156f
hayoade1 <44470559+hayoade1@users.noreply.github.com>
2020-02-16 15:33:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,48 +1,72 @@
-provider "azurerm" {
-  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
- 
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+variable "prefix" {
+  default = "tfvmex"
 }
 
-resource "azurerm_mysql_server" "server" {
-  name                = "${var.server_name}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "West US 2"
+}
 
-  sku {
-    name     = "B_Gen5_2"
-    capacity = 2
-    tier     = "Basic"
-    family   = "Gen5"
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
   }
+}
 
-  storage_profile {
-    storage_mb            = 5120
-    backup_retention_days = 7
-    geo_redundant_backup  = "Disabled"
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
-
-  administrator_login          = "${random_string.username.result}"
-  administrator_login_password = "${random_string.password.result}"
-  version                      = "5.7"
-  ssl_enforcement              = "Disabled"
-}
-
-resource "azurerm_mysql_database" "db" {
-  name                = "${var.db_name}"
-  resource_group_name = "${var.resource_group}"
-  server_name         = "${azurerm_mysql_server.server.name}"
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
-
-resource "azurerm_mysql_firewall_rule" "fw_rule_vault" {
-  name                = "${var.service_name}-${var.server_name}-vault"
-  resource_group_name = "${var.resource_group}"
-  server_name         = "${azurerm_mysql_server.server.name}"
-  start_ip_address    = "${var.vault_cluster}"
-  end_ip_address      = "${var.vault_cluster}"
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "${random_string.username.result}"
+    admin_password = "${random_string.password.result}"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
 }
