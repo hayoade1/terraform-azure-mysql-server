641deded7ea3b07a6a78cebbf65b688f85849cc1
hayoade1 <44470559+hayoade1@users.noreply.github.com>
2020-02-16 15:34:38 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,1 @@
-output "fqdn" {
-  value = "${azurerm_mysql_server.server.fqdn}"
-}
 
-output "server_name" {
-  value = "${var.server_name}"
-}
-
-output "db_name" {
-  value = "${var.db_name}"
-}
-
-output "vault_mount" {
-  value = "${vault_mount.db.path}"
-}
