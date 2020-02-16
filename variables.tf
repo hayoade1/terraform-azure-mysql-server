8634efe0cbc1f7c077f63748e9a540b75756769b
hayoade1 <44470559+hayoade1@users.noreply.github.com>
2020-02-16 14:59:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,33 +6,10 @@ variable "subscription_id" {}
 
 variable "tenant_id" {}
 
-variable "service_name" {
-    default = "nextgen"
-  }
-
-variable "db_name" {
-  default = "nextgeniam"
-  }
-
 variable "resource_group" {
   default = "nextgenresource"
   }
 
-variable "server_name" {
-  default = "nextgeniamserver"
-  }
-
-variable "location" {
-  default = "East US"
-  }
-
-variable "max_ttl" {
-  default = "90"
-}
-
-variable "default_ttl" {
-  default = "30"
-}
 
 variable "vault_addr" {}
 variable "vault_token" {}
