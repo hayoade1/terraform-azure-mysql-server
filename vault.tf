50573568b29ebd3a93964834ecfd8a8703165a35
hayoade1 <44470559+hayoade1@users.noreply.github.com>
2020-02-16 18:59:52 -0500

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,11 +1,12 @@
 
 resource "vault_generic_secret" "scalability" {
-  path = "kv/secret/scalability"
+  count = 2
+  path = "kv/secret/scalability${count.index}"
 
   data_json = <<EOT
 {
-  "username":   "${random_string.username.result}",
-  "password": "${random_string.password.result}"
+  "username":   "${random_string.username.result.count.index}",
+  "password": "${random_string.password.result.count.index}"
 }
 EOT
 }
